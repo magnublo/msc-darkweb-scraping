32c034efa64dc28350b467114a91d4535119e5cf
magnus <magnublo@stud.ntnu.no>
2019-09-27 14:20:50 +0200

    fixed default values for empty input in main.py

--- src/main.py
+++ src/main.py
@@ -6,12 +6,18 @@ listing_nr = input("Listing_nr from which to start: ")
 
 if session_id:
     session_id = int(session_id)
+else:
+    session_id = None
 
 if pagenr:
     pagenr = int(pagenr)
+else:
+    pagenr = 1
 
 if listing_nr:
     listing_nr = int(listing_nr)
+else:
+    listing_nr = 0
 
 scraping_session = EmpireScrapingSession(session_id=session_id, initial_pagenr=pagenr, initial_listingnr=listing_nr)
 scraping_session.scrape()
