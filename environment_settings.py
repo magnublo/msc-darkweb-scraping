8c8eee917a34ff23d55568108b1c538eac22668c
magnus <magnublo@stud.ntnu.no>
2019-10-27 12:30:24 +0100

    added environment_settings.py to .gitignore

--- environment_settings.py
+++
@@ -1,8 +0,0 @@
-MYSQL_ECHO_DEBUG = False
-DEBUG_MODE = True
-TOR_PROXY_SERVER_ADDRESS = "localhost"
-LOWEST_TOR_PORT = 9050
-DB_USERNAME = "msc-scraper"
-DB_ENGINE_BASE_URL = 'mysql+mysqlconnector://' + DB_USERNAME + ':Password123!@localhost:3306/magnublo_scraping'
-
-LOCAL_ENVIRONMENT_HANDLERS = {}
