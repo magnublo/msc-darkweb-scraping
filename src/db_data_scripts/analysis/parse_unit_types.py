dfb20bffe8f47fc95f9274296b9ddb82f0521a56
Magnus <magnus@localhost.localdomain>
2020-02-25 20:26:41 +0100

    workbench commit

--- src/db_data_scripts/analysis/parse_unit_types.py
+++ src/db_data_scripts/analysis/parse_unit_types.py
@@ -21,21 +21,12 @@ for title, unit_type in titles_and_unit_types:
     all_unit_types.add((title, unit_type))
     match = re.match(r"([0-9.,]*|[0-9]+\/[0-9])-?(g|grams|gram|gr)$", unit_type, flags=re.IGNORECASE)
     if match:
-        start_index = match.regs[1][0]
-        end_index = match.regs[1][1]
-        numerical_expression = unit_type[start_index:end_index]
-        if numerical_expression.find("/") != -1:
-            # fraction
-            numerator, denominator = [int(s) for s in numerical_expression.split("/")]
-            grams = numerator / denominator
-        else:
-            # decimal
-            numerical_expression = numerical_expression if numerical_expression else "1"
-            grams = parse_float(numerical_expression)
-
+        grams = get_grams(unit_type, match, grams_per_unit=1)
         parsed_unit_types.add((title, unit_type))
         parsed_types_and_nr_of_grams.append((unit_type, str(grams)))
         continue
+        start_index = match.regs[1][0]
+
 
     match = re.match(r"([0-9.,]*|[0-9]+\/[0-9])-?(mg|mgrams|mgram|milligrams|mgs)$", unit_type, flags=re.IGNORECASE)
 
@@ -85,11 +76,11 @@ for title, unit_type in titles_and_unit_types:
         if numerical_expression.find("/") != -1:
             # fraction
             numerator, denominator = [int(s) for s in numerical_expression.split("/")]
-            grams = (numerator / denominator) * 1000
+            grams = (numerator / denominator) * (10**-6)
         else:
             # decimal
             numerical_expression = numerical_expression if numerical_expression else "1"
-            grams = parse_float(numerical_expression) * 1000
+            grams = parse_float(numerical_expression) * (10**-6)
 
         parsed_unit_types.add((title, unit_type))
         parsed_types_and_nr_of_grams.append((unit_type, str(grams)))
@@ -99,5 +90,5 @@ unparsed_unit_types = all_unit_types.difference(parsed_unit_types)
 printTable(list([(s[1], s[0]) for s in unparsed_unit_types]))
 printTable(parsed_types_and_nr_of_grams)
 #print("\n".join(["\t\t\t\t\t\t".join(s) for s in unparsed_unit_types]))
-
+print(len(parsed_unit_types)/len(all_unit_types))
 # UPDATE `magnublo_scraping`.`listing_observation` SET `is_weight_unit_type` = '1', `grams_per_unit` = '5' WHERE (`id` = '16');
